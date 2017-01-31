bdec04b52ffdcc4d6dd1230b99b9bb0e5519f8fe
Akshay Darekar <acdarekar@gmail.com>
2017-01-31 11:00:00 +0530

    add view that renders the form

--- evaluation/apps/student/views.py
+++ evaluation/apps/student/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django import forms
+from .forms import StudentForm
 
 # Create your views here.
+
+
+def add_student(request):
+    if request.method == 'GET':
+        form = StudentForm()
+    else:
+        form = StudentForm(request.POST)
+
+    # import ipdb; ipdb.set_trace()
+
+    if form.is_valid():
+        pass
+
+    return render(request, 'base.html', {'form': form})
