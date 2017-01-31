6b5e02845f5a7f728090fd46dceb36c01cffc04d
Akshay Darekar <acdarekar@gmail.com>
2017-01-31 11:31:37 +0530

    save the form

--- evaluation/apps/student/views.py
+++ evaluation/apps/student/views.py
@@ -1,19 +1,18 @@
-from django.shortcuts import render
-from django import forms
+from django.shortcuts import render, redirect
 from .forms import StudentForm
+from .models import Student
 
 # Create your views here.
 
 
 def add_student(request):
+    student_data = Student.objects.all()
     if request.method == 'GET':
         form = StudentForm()
     else:
         form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            redirect('/addstudent')
 
-    # import ipdb; ipdb.set_trace()
-
-    if form.is_valid():
-        pass
-
-    return render(request, 'base.html', {'form': form})
+    return render(request, 'base.html', {'form': form, 'student_data': student_data})
