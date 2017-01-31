5840eacbe8fa222d3ad512b23b738ca8339e7017
Akshay Darekar <acdarekar@gmail.com>
2017-01-31 10:58:23 +0530

    update fields; create migration

--- evaluation/apps/student/models.py
+++ evaluation/apps/student/models.py
@@ -3,14 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Student(models.model):
+class Student(models.Model):
     """Base Class for Model"""
     name = models.CharField(max_length=99)
     standard = models.CharField(max_length=10)
-    marks = models.IntegerField(required=True)
+    marks = models.CharField(max_length=10)
 
     class Meta:
         verbose_name = "Student"
         verbose_name_plural = "Students"
-
-
